251e94b23308273c12d4743de5bcce2a752af5aa
Brett T. Warden <4c0e8e88@tempmail.wgz.org>
2019-04-01 21:15:36 -0700

    Implement code reader/storer

---
+++ test.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: test.pl
+#
+#        USAGE: ./test.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Brett T. Warden (btw), bwarden@wgz.com
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 03/21/2019 10:29:25 PM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+use Modern::Perl;
+
+use DBI;
+use IO::Select;
+use Linux::USBKeyboard;
+
+my $db = '/home/bwarden/src/barcode-scanner-input/barcodes.db';
+
+my $vendor_id  = 0x0581;
+my $product_id = 0x0103;
+ 
+my @bpid = (0x0581, 0x0103); # barcode reader
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$db", '', '')
+  or die "Couldn't open DB $db\n";
+
+$dbh->do('CREATE TABLE IF NOT EXISTS scans(id INTEGER PRIMARY KEY, code TEXT, type TEXT, source INTEGER, date_added DATETIME DEFAULT CURRENT_TIMESTAMP);');
+my $sth = $dbh->prepare('INSERT INTO scans (code, source) VALUES(?, ?);')
+  or die $dbh->errstr;
+ 
+my $bp = Linux::USBKeyboard->open(@bpid)
+  or die "Couldn't grab device @bpid\n";
+
+my $sel = IO::Select->new;
+$sel->add($bp);
+ 
+my %buffer;
+while(my @ready = $sel->can_read) {
+  #warn "ready count: ", scalar(@ready);
+  foreach my $fh (@ready) {
+    my $v;
+    if(1 && $fh == $bp) { # treat linewise
+      chomp($v = <$fh>);
+      print $fh->pid, ' says: ', $v, "\n";
+      $sth->execute($v, $fh->pid);
+    }
+    else { # charwise
+      $v = getc($fh);
+      if ($v ne "\n") {
+        push(@{$buffer{$fh->pid}}, $v);
+      }
+      else
+      {
+        if ($buffer{$fh->pid} and ref $buffer{$fh->pid} eq 'ARRAY') {
+          local $| = 1;
+          my $code = join('', @{$buffer{$fh->pid}});
+          print $fh->pid, ' says: ', $code, "\n";
+          $sth->execute($code, $fh->pid);
+        }
+        @{$buffer{$fh->pid}} = ();
+      }
+    }
+  }
+}
+ 
+ 
+# vim:ts=2:sw=2:et:sta
